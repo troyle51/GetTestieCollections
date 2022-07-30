eb27a35936bd423ae7a037f4f28c724fa4c18024
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-30 19:03:34 -0400

    map test

--- gettestie/src/test/java/rocks/zipcode/TestMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestMap.java
@@ -1,4 +1,46 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMap {
+
+    Map<Integer, Integer> hashMap;
+
+    @Before
+    public void init(){
+        hashMap = new HashMap<>();
+    }
+
+    @Test
+    public void testAdd(){
+        hashMap.put(1,1);
+        int expected = hashMap.get(1);
+
+        int actual = 1;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmpty(){
+        Assert.assertEquals(true, hashMap.isEmpty());
+    }
+
+    @Test
+    public void testRemove(){
+        hashMap.put(1,1);
+        hashMap.put(2,1);
+        hashMap.put(3,1);
+        hashMap.put(4,1);
+
+        hashMap.remove(2);
+        Assert.assertEquals(3, hashMap.size());
+    }
+
+
 }
